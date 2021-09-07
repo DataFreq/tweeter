c9057341c5208c61cb020cd06b0f902049c003ac
vagrant <rpg7ofdeath@gmail.com>
2021-09-07 23:32:15 +0000

    added char count function

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $(".tweetarea").keyup(function(e) {
+    const charLength = e.target.value.length;
+    $(".counter").text (140 - charLength);
+    if (charLength > 140) {
+      $('.counter').css('color', 'red');
+    } else {
+      $('.counter').css('color', 'black');
+    }
+  });
+});
