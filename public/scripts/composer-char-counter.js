4ec11723222b5db4a8cbaf4e05411a5a27f530e8
vagrant <rpg7ofdeath@gmail.com>
2021-09-08 22:19:55 +0000

    linted

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   $(".tweetarea").keyup(function(e) {
     const charLength = e.target.value.length;
-    $(".counter").text (140 - charLength);
+    $(".counter").text(140 - charLength);
     if (charLength > 140) {
       $('.counter').css('color', 'red');
     } else {
