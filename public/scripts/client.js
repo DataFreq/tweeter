0fe7ea03d681a65414526d88096f12f3b79cffdb
vagrant <rpg7ofdeath@gmail.com>
2021-09-09 01:53:48 +0000

    refactored code for better readability

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -15,24 +15,25 @@ $(document).ready(function () {
     const name = data.user.name;
     const handle = data.user.handle;
     const tweet = data.content.text;
-    //create full tweet element
-    const $tweetData = $("<article>").addClass("tweet");
     //create header Element
     const $header = $("<header>");
-    const $createAvatar = $('<img>').addClass("tweet-avatar").attr("src",userAvatar);
-    const $createName = $('<p>').addClass("name").text(name);
-    const $createHandle = $('<p>').addClass("username").text(handle);
+    $header.append(
+      $('<img>').addClass("tweet-avatar").attr("src",userAvatar),
+      $('<p>').addClass("name").text(name),
+      $('<p>').addClass("username").text(handle)
+    );
     //create tweet element
     const $createTweet = $('<p>').addClass("user-tweet").text(tweet);
     //create footer element
     const $footer = $("<footer>");
-    const $tweetTime = $("<p>").text(tweetTime);
-    const $flag = $("<i>").addClass("fas fa-flag");
-    const $retweet = $("<i>").addClass("fas fa-retweet");
-    const $heart = $("<i>").addClass("fas fa-heart");
-    //adding to tweet element
-    $header.append($createAvatar, $createName, $createHandle);
-    $footer.append($tweetTime, $flag, $retweet, $heart);
+    $footer.append(
+      $("<p>").text(tweetTime),
+      $("<i>").addClass("fas fa-flag"),
+      $("<i>").addClass("fas fa-retweet"),
+      $("<i>").addClass("fas fa-heart")
+    );
+    //create full tweet element
+    const $tweetData = $("<article>").addClass("tweet");
     $tweetData.append($header, $createTweet, $footer);
     return $tweetData;
   };
