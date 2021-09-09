fefa19380a8338e48468fc0a33c945096862d7b3
vagrant <rpg7ofdeath@gmail.com>
2021-09-09 01:45:49 +0000

    changed how tweet element is created (string literal >> jQuery) and prevented XSS with .text

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -11,23 +11,30 @@ $(document).ready(function () {
   };
   const createTweetElement = (data) => {
     const tweetTime = timeago.format(data.created_at);
-    const markup = `
-      <article class="tweet">
-        <header>
-          <img class="tweet-avatar" src="${data.user.avatars}">
-          <p class="name">${data.user.name}</p>
-          <p class="username">${data.user.handle}</p>
-        </header>
-          <p class="user-tweet">${data.content.text}</p>
-        <footer>
-          <p>${tweetTime}</p>
-          <i class="fas fa-flag"></i>
-          <i class="fas fa-retweet"></i>
-          <i class="fas fa-heart"></i>
-        </footer>
-      </article>
-    `;
-    return markup;
+    const userAvatar = data.user.avatars;
+    const name = data.user.name;
+    const handle = data.user.handle;
+    const tweet = data.content.text;
+    //create full tweet element
+    const $tweetData = $("<article>").addClass("tweet");
+    //create header Element
+    const $header = $("<header>");
+    const $createAvatar = $('<img>').addClass("tweet-avatar").attr("src",userAvatar);
+    const $createName = $('<p>').addClass("name").text(name);
+    const $createHandle = $('<p>').addClass("username").text(handle);
+    //create tweet element
+    const $createTweet = $('<p>').addClass("user-tweet").text(tweet);
+    //create footer element
+    const $footer = $("<footer>");
+    const $tweetTime = $("<p>").text(tweetTime);
+    const $flag = $("<i>").addClass("fas fa-flag");
+    const $retweet = $("<i>").addClass("fas fa-retweet");
+    const $heart = $("<i>").addClass("fas fa-heart");
+    //adding to tweet element
+    $header.append($createAvatar, $createName, $createHandle);
+    $footer.append($tweetTime, $flag, $retweet, $heart);
+    $tweetData.append($header, $createTweet, $footer);
+    return $tweetData;
   };
   $("form").submit((event) => {
     event.preventDefault();
